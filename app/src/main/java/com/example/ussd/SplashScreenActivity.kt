33ca8f3ca7532ecbd304b3a63f4d8c74032f6ce2
Murodillo Ismatov <ismatovmurodillo@gmail.com>
2021-07-29 11:18:11 +0500

    name changed

--- app/src/main/java/com/example/ussd/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ussd/SplashScreenActivity.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
         Handler().postDelayed({
             val intent = Intent(this, Umumiy_diller::class.java)
             startActivity(intent)
             finish()
         }, 3000)
     }
-    }
+}
